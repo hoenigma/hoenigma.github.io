429f0af9faaf64f5cb9b9d634fc7f806f2a81699
Matthew <mhoenig35@gmail.com>
2024-05-14 19:33:02 +0100

    added a carousel to the projects

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// ------------Navbar---------------
+
 document.addEventListener("DOMContentLoaded", function () {
   const burger = document.querySelector(".navbar-burger");
   const menu = document.querySelector(".navbar-menu");
@@ -6,4 +8,43 @@ document.addEventListener("DOMContentLoaded", function () {
     burger.classList.toggle("is-active");
     menu.classList.toggle("is-active");
   });
+});
+
+// -----------Carousel------------
+
+document.addEventListener("DOMContentLoaded", function () {
+  const carousel = document.getElementById("carousel");
+  const items = carousel.querySelectorAll(".carousel-item");
+  const prevButton = carousel.querySelector(".carousel-prev");
+  const nextButton = carousel.querySelector(".carousel-next");
+
+  let currentSlide = 0;
+
+  function showSlide(slideIndex) {
+    items.forEach((item, index) => {
+      if (index === slideIndex) {
+        item.classList.add("is-active");
+      } else {
+        item.classList.remove("is-active");
+      }
+    });
+  }
+
+  function goToNextSlide() {
+    currentSlide = (currentSlide + 1) % items.length;
+    showSlide(currentSlide);
+    console.log(currentSlide);
+  }
+
+  function goToPrevSlide() {
+    currentSlide = (currentSlide - 1 + items.length) % items.length;
+    showSlide(currentSlide);
+    console.log(currentSlide);
+  }
+
+  prevButton.addEventListener("click", goToPrevSlide);
+  nextButton.addEventListener("click", goToNextSlide);
+
+  // Show initial slide
+  showSlide(currentSlide);
 });
